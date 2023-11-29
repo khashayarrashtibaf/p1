232e133f48aeb041bc77dea7839a7c7d6d5742a6
khashayarrashtibaf <152330630+khashayarrashtibaf@users.noreply.github.com>
2023-11-29 10:32:28 +0330

    Add files via upload

---
+++ assignment2p1.py
@@ -0,0 +1,31 @@
+x = input("Which One(* or / or + or -) :" )
+a = float(input("Enter Number: "))
+b = float(input("Enter 2d Number: "))
+
+if x == "*":
+    
+    print(a*b)
+
+
+if x == "/":
+    
+    print(a/b)
+
+if x == "+":
+    print(a+b)
+
+if x == "-":
+    print(a-b)
+
+    
+
+
+    
+    
+    
+
+
+
+
+
+
---
+++ assignment2p2.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+
+if a+b>c and a+c>b and b+c>a :
+    print("Triangle can be created")
+
+else:
+    print("Triangle can not be created! ") 
